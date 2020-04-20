b9c1416f9bd70cef7e8716d52c63ec15007bc026
oleg <olegys93@gmail.com>
2020-04-21 00:06:21 +0300

    updated server

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 
 const PORT = process.env.PORT || 80;
 
+app.get('/', () => {
+    res.end('<h1>test run</h1>');
+});
+
 app.listen(PORT, () => {
     console.log('Server has been started !!!');
 });
