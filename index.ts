dcde88d63fcb6a8bb47b171468f0219a74f71ede
oleg <olegys93@gmail.com>
2020-04-22 00:44:34 +0300

    test ci1

--- index.ts
+++ index.ts
@@ -1,4 +1,18 @@
 import express from 'express';
+import { MongoClient } from 'mongodb';
+
+const mongoClient = new MongoClient('mongodb+srv://olegys:<password>@clouddb-w19nf.gcp.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
+mongoClient.connect(function (err, client) {
+
+    if (err) {
+        return console.log(err);
+    }
+    
+
+    console.log('connect to db !');
+
+    client.close();
+});
 
 const server = express();
 
